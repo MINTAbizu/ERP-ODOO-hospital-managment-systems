8e6b1c8b32bf976c1df1e5d2724a565d9d4df434
mintesinot bizuayehw <150599734+MINTAbizu@users.noreply.github.com>
2026-07-22 11:23:00 -0700

    bed managment

---
+++ hms/models/bedmanagement.py
@@ -0,0 +1,23 @@
+
+from odoo import api, fields, models
+
+
+class bedmanagement(models.Model):
+    _name="bed.management"
+    
+    _description="bed management "
+    inherit = ["mail.thread", "mail.activity.mixin"]
+    # _rec_name='patient_id'
+    
+    patient_id=fields.Many2one("hospital.patient" ,string="patient")
+    name= fields.Char(string="bed name")
+    bed_capacity= fields.Char(string="capacity")
+    
+    ward_id= fields.Many2one("doctor.descrption",string="ward assigned doctor")
+    
+    location_bed=fields.Char(string="bed location")
+    bed_type= fields.Selection([("single","single"),("double","double"),("vip","Vip")])
+    bed_status=fields.Selection([("freee","free"),("occupied","occupied")  ,("maintenance","maintenance")])
+    
+    
+    
